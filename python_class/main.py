e2178af58174e6c62b5e8ebfcc5f361b5f2b98b9
ariana <arishahamidova@gmail.com>
2025-04-09 12:11:47 +0500

    added QHBoxLayout

--- python_class/main.py
+++ python_class/main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QTextEdit, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QTextEdit, QVBoxLayout, QHBoxLayout, QWidget
 
 app = QApplication(sys.argv)
 
