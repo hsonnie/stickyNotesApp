8f02d0e67b320c0b62c3f24ffc202fc797aa6a22
ariana <arishahamidova@gmail.com>
2025-04-09 12:10:49 +0500

    add the button

--- python_class/main.py
+++ python_class/main.py
@@ -14,6 +14,16 @@ class NoteWindow(QWidget):
             | Qt.WindowType.WindowStaysOnTopHint)
         self.setStyleSheet("background: #FFFF99; color: #62622f; border: 0; font-size: 16pt;")
         layout = QVBoxLayout()
+
+        buttons = QHBoxLayout()
+        self.close_btn = QPushButton("x")
+        self.close_btn.setStyleSheet("font-weight: bold; font-size: 25px; width: 25px: hight: 25px;")
+        self.close_btn.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.close_btn.clicked.connect(self.close)
+        buttons.addStretch()
+        buttons.addWigdet(self.close_btn)
+        layout.addLayout(buttons)
+
         self.text = QTextEdit()
         layout.addWidget(self.text)
         self.setLayout(layout)
@@ -22,4 +32,3 @@ note = NoteWindow()
 note.show()
 app.exec()
 
-#hello 
